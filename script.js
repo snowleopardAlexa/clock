ce2d1d089a0cf28618061c3177c0c2ea5307ea63
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-03 19:46:51 -0400

    add function setClock

---
+++ script.js
@@ -0,0 +1,8 @@
+setinterval(setClock, 1000)
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = currentDate.getMinutes() / 60
+    const hoursRatio = currentDate.getHours() / 60
+}
